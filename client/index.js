7ce330c87277ea1de4824b293f6d892ae67cf349
Gabriel Rumbaut <grumbaut@gmail.com>
2018-03-01 18:54:02 -0500

    solutions

--- client/index.js
+++ client/index.js
@@ -4,4 +4,27 @@
 import whiteboard, {draw} from './whiteboard'
 
 // Example: Draw a single stroke.
-draw([0, 0], [250, 250], 'red', true)
+// draw([0, 0], [250, 250], 'red', true)
+
+var socket = io(window.location.origin);
+
+socket.on('connect', function() {
+  console.log('I have made a persistent two-way connection to the server!');
+});
+
+whiteboard.on('draw', (start, end, color) => {
+  socket.emit('doodle', {start, end, color});
+});
+
+socket.on('drawData', (event) => {
+  const { start, end, color} = event;
+  draw(start, end, color);
+});
+
+socket.on('state', (state)=> {
+  state.forEach(event => {
+    const { start, end, color } = event;
+    draw(start, end, color);
+  });
+});
+
--- server.js
+++ server.js
@@ -1,11 +1,30 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+const socketio = require('socket.io');
+var state = [];
 
-// app.listen() returns an http.Server object
-// http://expressjs.com/en/4x/api.html#app.listen
 const server = app.listen(1337, function () {
-    console.log(`Listening on http://localhost:${server.address().port}`);
+  console.log('The server is listening on port 1337!');
 });
+
+var io = socketio(server);
+
+io.on('connection', function (socket) {
+  console.log('A new client has connected!');
+  console.log(socket.id);
+  socket.on('disconnect', function() {
+    console.log(':-(');
+  });
+  if(state.length) {
+    socket.emit('state', state);
+  }
+  socket.on('doodle', event => {
+    state.push(event);
+    socket.broadcast.emit('drawData', event);
+  });
+});
+
+app.use(express.static(path.join(__dirname, 'browser')));
 
 app.use(express.static(path.join(__dirname, 'public')));
